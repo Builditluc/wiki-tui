bb35cef37e0e7308cba6860bf80fbbcf5e190b10
builditluc <37375448+Builditluc@users.noreply.github.com>
2021-01-23 12:39:10 +0100

    Implemented the wiki::Fetchable trait for WikiSql

--- src/db/wiki.rs
+++ src/db/wiki.rs
@@ -1,6 +1,10 @@
 use crate::db::api;
+use crate::traits::wiki::*;
+
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
+use crate::db::models::article_index::ArticleIndex;
+use uuid::Uuid;
 
 //TODO: Make WikiSQL implement the Wiki traits
 pub struct WikiSql {
@@ -24,5 +28,37 @@ impl WikiSql {
         info!("Successfully connected to {}", database_url);
 
         return connection;
+    }
+}
+
+impl Fetchable for WikiSql {
+    //TODO: Implement get_all_articles for WikiSql
+    fn get_all_articles(&self) -> Vec<ArticleIndex> {
+        unimplemented!()
+    }
+
+    //TODO: Implement get_article_by_id for WikiSql
+    fn get_article_by_id(&self, article_id: &Uuid) -> ArticleIndex {
+        unimplemented!()
+    }
+
+    //TODO: Implement get_article_by_page_id for WikiSql
+    fn get_article_by_page_id(&self, page_id: &i32) -> ArticleIndex {
+        unimplemented!()
+    }
+
+    //TODO: Implement get_article_by_title for WikiSql
+    fn get_article_by_title(&self, title: &String) -> ArticleIndex {
+        unimplemented!()
+    }
+
+    //TODO: Implement get_article_with_title for WikiSql
+    fn get_article_with_title(&self, title: &String) -> Vec<ArticleIndex> {
+        unimplemented!()
+    }
+
+    //TODO: Implement fetch_article for WikiSql
+    fn fetch_article(&self, index: &ArticleIndex) {
+        unimplemented!()
     }
 }
