2cf77c21c71d54294abddd32175834573c315d72
builditluc <37375448+Builditluc@users.noreply.github.com>
2021-02-23 14:50:37 +0100

    Removed everything (almost) This is the beginning of the new wiki-tui

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
 #[macro_use] extern crate log;
-#[macro_use] extern crate diesel;
 
-use crate::traits::wiki::Fetchable;
 
-pub mod db;
 pub mod tui;
 pub mod tests;
 pub mod logging;
-pub mod traits;
 
 fn main() {
     logging::Logger::new();
-
-    let wiki = db::wiki::WikiSql::new();
-    wiki.get_all_articles();
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,16 +3,4 @@ mod tests {
     fn it_works() {
         println!("Hello World!")
     }
-
-    #[test]
-    fn test_env() {
-        use dotenv::dotenv;
-        use std::env;
-
-        dotenv::dotenv().ok();
-
-        for (key, value) in env::vars() {
-            println!("{}: {}", key, value);
-        }
-    }
 }
