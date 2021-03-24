4782b861bcbc3b3b5599af6c034a656ac1f3850e
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-03-24 18:47:34 +0100

    Added more of the UI

--- src/tui.rs
+++ src/tui.rs
@@ -12,15 +12,24 @@ impl Tui {
     pub fn run(&self) {
         let mut siv = cursive::default();
 
-        let search_bar = EditView::new();
+        let search_bar = EditView::new()
+            .full_width();
         let search_button = Button::new("Search", |cb| {});
-
         let search_layout = LinearLayout::horizontal()
             .child(search_bar)
             .child(search_button);
 
-        siv.add_layer(Dialog::around(search_layout)
-                      .title("wiki-tui"));
+        let mut search_results = SelectView::new();
+        search_results.add_item("Article 1", 1);
+        search_results.add_item("Article 2", 2);
+        let search_results = search_results.full_screen();
+
+        siv.add_fullscreen_layer(Dialog::around(LinearLayout::vertical()
+                                                .child(search_layout)
+                                                .child(search_results))
+                      .title("wiki-tui")
+                      .button("Quit", Cursive::quit)
+                      .full_screen());
 
         siv.run();
     }
