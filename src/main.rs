7b6bd6166127bc4fd63a89b93030c7ce5a5fd1c2
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-03-25 19:16:51 +0100

    Moved the UI stuff into main.rs and fixed some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use] extern crate log;
 extern crate ini;
 
-pub mod tui;
+use cursive::Cursive;
+use cursive::views::*;
+use cursive::traits::*;
+
 pub mod tests;
 pub mod logging;
 pub mod wiki;
@@ -9,6 +12,29 @@ pub mod wiki;
 fn main() {
     logging::Logger::new();
 
-    let ui = tui::Tui::new();
-    ui.run();
+    let mut siv = cursive::default();
+    siv.add_global_callback('q', Cursive::quit);
+
+    let search_bar = EditView::new().full_width();
+    let search_button = Button::new("Search", |s| {on_search(s)});
+    let search_layout = Dialog::around(LinearLayout::horizontal()
+                                       .child(search_bar)
+                                       .child(search_button));
+
+    let mut search_results = SelectView::new();
+    search_results.add_item("Article 1", 1);
+    search_results.add_item("Article 2", 2);
+    let search_results = search_results.full_screen();
+
+    siv.add_fullscreen_layer(Dialog::around(LinearLayout::vertical()
+                                            .child(search_layout)
+                                            .child(Dialog::around(search_results)))
+                             .title("wiki-tui")
+                             .button("Quit", Cursive::quit)
+                             .full_screen());
+    siv.run();
+}
+
+fn on_search(siv: &mut Cursive) {
+
 }
