ecdfd4514354b8736149bf8bcdf3f7be5c0ab73f
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-04-02 15:25:01 +0200

    removes shell.nix

--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs ? import <nixpkgs> {}  }:
-pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
-          
-          buildInputs = with pkgs; [ openssl ncurses ];
-   }
