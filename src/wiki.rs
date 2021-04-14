419be8f9510bc4492ca916dff1b9398b73d49f89
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-04-14 09:34:58 +0200

    fixed Wiki::search_articles() and continue_search in main.rs

--- src/wiki.rs
+++ src/wiki.rs
@@ -31,12 +31,12 @@ impl Wiki {
     }
     
     fn search_articles(&self, title: &str, continue_code: Option<&ContinueCode>) -> SearchResponse {
-        let url = format!("{}?action=query&list=search&srwhat=text&srsearch={}&format=json", self.api_config.base_url.clone(), title);
+        let mut url = format!("{}?action=query&list=search&srwhat=text&srsearch={}&format=json", self.api_config.base_url.clone(), title);
         if continue_code.is_some() {
             let continue_unwrapped = continue_code.unwrap();
             let continue_code_unwrapped = &continue_unwrapped.continue_code;
             let continue_scroll_offset_unwrapped = continue_unwrapped.scroll_offset;
-            let _url = format!("{}?action=query&list=search&srwhat=text&srsearch={}&format=json&continue={}&sroffset={}", self.api_config.base_url.clone(), title, continue_code_unwrapped, continue_scroll_offset_unwrapped);
+            url = format!("{}?action=query&list=search&srwhat=text&srsearch={}&format=json&continue={}&sroffset={}", self.api_config.base_url.clone(), title, continue_code_unwrapped, continue_scroll_offset_unwrapped);
         }
 
         self.client.get(&url)
