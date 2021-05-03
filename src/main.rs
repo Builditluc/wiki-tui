e4c0b490590073ddb7aedf38a64e1814237a00cf
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-05-03 07:21:59 +0200

    Refactored the on_search function

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 #[allow(unused_imports)]
-#[macro_use] extern crate log;
-extern crate ini;
+#[macro_use]
+extern crate log;
 extern crate anyhow;
+extern crate ini;
 
-use cursive::Cursive;
-use cursive::views::*;
-use cursive::traits::*;
-use cursive::theme::*;
-use cursive::utils::*;
-use cursive::event::*;
-use cursive::view::{Scrollable, Resizable};
 use anyhow::*;
+use cursive::event::*;
+use cursive::theme::*;
+use cursive::traits::*;
+use cursive::utils::*;
+use cursive::view::{Resizable, Scrollable};
+use cursive::views::*;
+use cursive::Cursive;
 
-pub mod tests;
-pub mod logging;
-pub mod wiki;
-pub mod structs;
 pub mod config;
+pub mod logging;
+pub mod structs;
+pub mod tests;
+pub mod wiki;
 
 pub const LOGO: &str = "
    _      __   (_)   / /__   (_)         / /_  __  __   (_)
@@ -34,15 +35,11 @@ fn main() {
     siv.add_global_callback('q', Cursive::quit);
 
     siv.set_user_data(wiki);
-    
     let search_bar = EditView::new()
-        .on_submit(|s, q| { 
-            on_search(s, q.to_string())
-        })
+        .on_submit(|s, q| on_search(s, q.to_string()))
         .with_name("search")
         .full_width();
-    let search_layout = Dialog::around(LinearLayout::horizontal()
-                                       .child(search_bar))
+    let search_layout = Dialog::around(LinearLayout::horizontal().child(search_bar))
         .title("Search")
         .title_position(cursive::align::HAlign::Left);
 
@@ -52,23 +49,27 @@ fn main() {
         .full_screen()
         .scrollable();
 
-    siv.add_fullscreen_layer(Dialog::around(LinearLayout::vertical()
-                                            .child(search_layout)
-                                            .child(Dialog::around(article_view)))
-                             .title("wiki-tui")
-                             .button("Quit", Cursive::quit)
-                             .full_screen());
-//  // This is just for testing    
-//    siv.cb_sink().send(Box::new(|s| {
-//        let test_preview = structs::wiki::ArticleResultPreview {
-//            page_id: 18630637,
-//            snippet: String::new(),
-//            title: String::new(),
-//        };
-//        s.add_layer(TextView::new("Test"));
-//        on_article_submit(s, &test_preview);
-//    }));
-//
+    siv.add_fullscreen_layer(
+        Dialog::around(
+            LinearLayout::vertical()
+                .child(search_layout)
+                .child(Dialog::around(article_view)),
+        )
+        .title("wiki-tui")
+        .button("Quit", Cursive::quit)
+        .full_screen(),
+    );
+    //  // This is just for testing
+    //    siv.cb_sink().send(Box::new(|s| {
+    //        let test_preview = structs::wiki::ArticleResultPreview {
+    //            page_id: 18630637,
+    //            snippet: String::new(),
+    //            title: String::new(),
+    //        };
+    //        s.add_layer(TextView::new("Test"));
+    //        on_article_submit(s, &test_preview);
+    //    }));
+    //
     siv.run();
 }
 
@@ -85,44 +86,53 @@ fn on_search(siv: &mut Cursive, search_query: String) {
 
     let search_response = wiki.search(&search_query);
 
-    let mut results_view = SelectView::<structs::wiki::ArticleResultPreview>::new()
-        .on_select(|s, item| {on_result_select(s, item)})
-        .on_submit(|s, a| {on_article_submit(s, a)});
+    let mut search_results_view = SelectView::<structs::wiki::ArticleResultPreview>::new()
+        .on_select(|s, item| on_result_select(s, item))
+        .on_submit(|s, a| on_article_submit(s, a));
 
-    let results_preview = TextView::new("")
+    let search_results_preview = TextView::new("")
         .h_align(cursive::align::HAlign::Left)
         .with_name("results_preview")
         .fixed_width(50);
-
 
     // convert the search results into Article Result Previews
     // and then add them to the results_view
     for search_result in search_response.query.search.clone() {
         let search_result = structs::wiki::ArticleResultPreview::from(search_result);
-        results_view.add_item(search_result.title.to_string(), search_result);
+        search_results_view.add_item(search_result.title.to_string(), search_result);
     }
 
+    let search_details_view = TextView::new(format!(
+        "Found {} articles matching your search",
+        &search_response.clone().query.search_info.total_hits
+    ));
+
     let query = search_query.to_string();
-
-    let search_info = TextView::new(format!("Found {} articles matching your search", &search_response.clone().query.search_info.total_hits));
-    let continue_button = Button::new("Show more results...", move |s| {
+    let search_continue_button = Button::new("Show more results...", move |s| {
         continue_search(s, query.clone(), &search_response.continue_code)
-    }).with_name("continue_button");
+    })
+    .with_name("continue_button");
 
-    let results_layout = LinearLayout::horizontal()
-        .child(Dialog::around(LinearLayout::vertical()
-                              .child(results_view.with_name("results_view").scrollable())
-                              .child(continue_button)))
-        .child(Dialog::around(results_preview));
+    let search_results_layout = LinearLayout::horizontal()
+        .child(Dialog::around(
+            LinearLayout::vertical()
+                .child(search_results_view.with_name("results_view").scrollable())
+                .child(search_continue_button),
+        ))
+        .child(Dialog::around(search_results_preview));
 
-    siv.add_layer(Dialog::around(LinearLayout::vertical()
-                                 .child(results_layout)
-                                 .child(search_info))
-                  .title(format!("Results for {}", search_query))
-                  .dismiss_button("Back")
-                  .button("Quit", Cursive::quit)
-                  .max_height(20));
- }
+    siv.add_layer(
+        Dialog::around(
+            LinearLayout::vertical()
+                .child(search_results_layout)
+                .child(search_details_view),
+        )
+        .title(format!("Results for {}", search_query))
+        .dismiss_button("Back")
+        .button("Quit", Cursive::quit)
+        .max_height(20),
+    );
+}
 
 fn on_result_select(siv: &mut Cursive, item: &structs::wiki::ArticleResultPreview) {
     let title = &item.title;
@@ -137,8 +147,11 @@ fn on_result_select(siv: &mut Cursive, item: &structs::wiki::ArticleResultPrevie
     for slice in split_snippet {
         if slice.contains("</span>") {
             let split_slice: Vec<&str> = slice.split("</span>").collect();
-            
-            styled_snippet.append(markup::StyledString::styled(split_slice[0], Color::Dark(BaseColor::Red)));
+
+            styled_snippet.append(markup::StyledString::styled(
+                split_slice[0],
+                Color::Dark(BaseColor::Red),
+            ));
             styled_snippet.append_plain(split_slice[1]);
         } else {
             styled_snippet.append_plain(slice);
@@ -170,19 +183,26 @@ fn on_article_submit(siv: &mut Cursive, article_preview: &structs::wiki::Article
         log::info!("Completed setting the content for the article view");
     });
 
-    let result = siv.focus_name("article").context("Failed to focus the article view");
+    let result = siv
+        .focus_name("article")
+        .context("Failed to focus the article view");
     match result {
         Ok(_) => log::info!("Successfully focussed the article view"),
         Err(error) => log::warn!("{:?}", error),
     }
-
 }
 
-fn continue_search(siv: &mut Cursive, search_query: String, continue_code: &structs::wiki::search::ContinueCode) {
+fn continue_search(
+    siv: &mut Cursive,
+    search_query: String,
+    continue_code: &structs::wiki::search::ContinueCode,
+) {
     let wiki: &wiki::Wiki = siv.user_data().unwrap();
     let search_response = wiki.continue_search(&search_query, continue_code);
 
-    let mut results_view = siv.find_name::<SelectView::<structs::wiki::ArticleResultPreview>>("results_view").unwrap();
+    let mut results_view = siv
+        .find_name::<SelectView<structs::wiki::ArticleResultPreview>>("results_view")
+        .unwrap();
     for search_result in search_response.query.search.clone() {
         let search_result = structs::wiki::ArticleResultPreview::from(search_result);
         results_view.add_item(search_result.title.clone(), search_result);
@@ -193,7 +213,9 @@ fn continue_search(siv: &mut Cursive, search_query: String, continue_code: &stru
         continue_search(s, search_query.clone(), &search_response.continue_code);
     });
 
-    let result = siv.focus_name("results_view").context("Failed to focus the results view");
+    let result = siv
+        .focus_name("results_view")
+        .context("Failed to focus the results view");
     match result {
         Ok(_) => log::info!("Successfully focussed the results view"),
         Err(error) => log::warn!("{:?}", error),
