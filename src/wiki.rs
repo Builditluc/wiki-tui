5c6a1605b3ee11852d3533c3f496d9b3fe89192f
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-05-04 14:05:50 +0200

    Refactored wiki.rs

--- src/wiki.rs
+++ src/wiki.rs
@@ -1,18 +1,20 @@
-use crate::structs::wiki::search::*;
+use crate::config::ApiConfig;
 use crate::structs::wiki::article::*;
 use crate::structs::wiki::parser::*;
+use crate::structs::wiki::search::*;
 use crate::structs::Parser;
-use crate::config::ApiConfig;
+use anyhow::*;
 
 pub struct Wiki {
     client: reqwest::blocking::Client,
     api_config: ApiConfig,
-    parser: Box<dyn Parser>
+    parser: Box<dyn Parser>,
 }
 
 impl Wiki {
     pub fn new(config: ApiConfig) -> Self {
-        let default_parser = Default{};
+        log::info!("Creating a instance of Wiki");
+        let default_parser = Default {};
         Wiki {
             client: reqwest::blocking::Client::new(),
             api_config: config,
@@ -23,19 +25,18 @@ impl Wiki {
     pub fn search(&self, title: &str) -> SearchResponse {
         self.search_articles(title, None)
     }
-
-    pub fn _get_article(&self, page_id: &i32) -> ArticleResponse {
-        let url = format!("{}?action=query&prop=extracts&pageids={}&formatversion=2&explaintext=true&exsectionformat=plain&format=json", self.api_config.base_url.clone(), page_id);
-        println!("{}", &url);
-        self.client.get(&url)
-            .send()
-            .unwrap()
-            .json::<ArticleResponse>()
-            .unwrap()
+    pub fn continue_search(&self, title: &str, continue_code: &ContinueCode) -> SearchResponse {
+        self.search_articles(title, Some(continue_code))
     }
-    
+
     fn search_articles(&self, title: &str, continue_code: Option<&ContinueCode>) -> SearchResponse {
-        let mut url = format!("{}?action=query&list=search&srwhat=text&srsearch={}&format=json", self.api_config.base_url.clone(), title);
+        // creating the url for the request
+        let mut url = format!(
+            "{}?action=query&list=search&srwhat=text&srsearch={}&format=json",
+            self.api_config.base_url.clone(),
+            title
+        );
+
         if continue_code.is_some() {
             let continue_unwrapped = continue_code.unwrap();
             let continue_code_unwrapped = &continue_unwrapped.continue_code;
@@ -43,22 +44,46 @@ impl Wiki {
             url = format!("{}?action=query&list=search&srwhat=text&srsearch={}&format=json&continue={}&sroffset={}", self.api_config.base_url.clone(), title, continue_code_unwrapped, continue_scroll_offset_unwrapped);
         }
 
-        self.client.get(&url)
+        // making the request
+        let response = match self
+            .client
+            .get(&url)
             .send()
-            .unwrap()
+            .context("Failed to send the request")
+        {
+            Ok(response) => {
+                log::info!("Successfully sent the request");
+                response
+            }
+            Err(error) => {
+                log::error!("Failed sending the request, {:?}", error);
+                panic!("Something happened, please check your logs")
+            }
+        };
+
+        // serializing the response
+        let serde_result = response
             .json::<SearchResponse>()
-            .unwrap()
-    }
-    pub fn continue_search(&self, title: &str, continue_code: &ContinueCode) -> SearchResponse {
-        self.search_articles(title, Some(continue_code))
+            .context("Failed serializing the response");
+
+        match serde_result {
+            Ok(result) => {
+                log::info!("Successfully serialized the response");
+                result
+            }
+            Err(error) => {
+                log::error!("Failed serializing the response, {:?}", error);
+                panic!("Something weird happened, please check your logs");
+            }
+        }
     }
 
     pub fn get_article(&self, page_id: &i32) -> Article {
+        // creating the url and making the request
         let url = format!("http://en.wikipedia.org/?curid={}", page_id);
-        let article_html = self.client.get(&url)
-            .send()
-            .unwrap();
+        let article_html = self.client.get(&url).send().unwrap();
 
+        // parsing the html response into a Article
         self.parser.parse(article_html)
     }
 }
