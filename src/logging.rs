1628052f844cd497b075a77733d6606bed3ebd20
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-05-04 20:02:35 +0200

    Replaced simple_logging with log4rs

--- src/logging.rs
+++ src/logging.rs
@@ -5,12 +5,25 @@ pub struct Logger;
 
 impl Logger {
     pub fn new(logging_config: &LoggingConfig) {
-        use simple_logging::log_to_file;
+        use log4rs::append::file::FileAppender;
+        use log4rs::config::{Appender, Config, Root};
+        use log4rs::encode::pattern::PatternEncoder;
 
-        let result = log_to_file(logging_config.log_output.clone(), logging_config.log_level).context("Failed to initialize the logging");
-        match result {
-            Ok(_) => log::info!("Successfully initialized the Logging Module"),
-            Err(error) => panic!("{:?}", error),
-        }
+        let wiki_tui = FileAppender::builder()
+            .encoder(Box::new(PatternEncoder::new(
+                "[{d(%Y-%m-%d %H:%M:%S)}] (({I})) [{h({l})}]  {m}\n",
+            )))
+            .build(logging_config.log_output.clone())
+            .unwrap();
+
+        let config = Config::builder()
+            .appender(Appender::builder().build("wiki_tui", Box::new(wiki_tui)))
+            .build(
+                Root::builder()
+                    .appender("wiki_tui")
+                    .build(logging_config.log_level.clone()),
+            )
+            .unwrap();
+        log4rs::init_config(config).unwrap();
     }
 }
