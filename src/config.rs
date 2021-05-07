58065db6ac9ec1b9d9a7e75d239f6573dbb14ebf
Builditluc <37375448+Builditluc@users.noreply.github.com>
2021-05-07 12:34:03 +0200

    Added log statements to config::Config

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,6 @@ use anyhow::*;
 use dirs;
 use ini::Ini;
 use lazy_static::*;
-use reqwest;
 use std::fs;
 use std::path::PathBuf;
 
@@ -34,6 +33,7 @@ impl Config {
         };
 
         // do the loading stuff here
+        info!("Loading the config");
         config.load_config();
 
         // return the config
@@ -48,17 +48,28 @@ impl Config {
         // check, if any errors occured during loading
         if config_exists.is_err() {
             // Abort the loading
+            warn!("Failed loading the config paths, {:?}", config_exists.err());
             return;
         }
 
         // read the config file and check if there were any errors
-        let config = match Ini::load_from_file(&self.config_path) {
-            Ok(config) => config,
-            Err(_) => return,
+        let config = match Ini::load_from_file(&self.config_path).context(format!(
+            "Failed loading the config file at the location: {}",
+            &self.config_path.to_str().unwrap_or("NONE")
+        )) {
+            Ok(config) => {
+                info!("Successfully loaded the config file");
+                config
+            }
+            Err(error) => {
+                warn!("{:?}", error);
+                return;
+            }
         };
 
         // if the config file exists, then load it
-        if config_exists.is_ok() && config_exists.unwrap() {
+        if config_exists.unwrap() {
+            info!("Loading the Api Config");
             self.load_api_config(&config);
         }
     }
@@ -76,16 +87,25 @@ impl Config {
 
         // create the app config folder if it doesn't exist
         if !app_config_dir.exists() {
-            fs::create_dir(app_config_dir);
+            info!("The app config directory doesn't exist, creating it now");
+            match fs::create_dir(app_config_dir).context("Couldn't create the app config directory")
+            {
+                Ok(_) => {
+                    info!("Successfully created the app config directory");
+                }
+                Err(error) => return Err(error),
+            };
         }
 
         // check, if the config file exists
         if !config_file_dir.exists() {
+            info!("The config file doesn't exist");
             return Ok(false);
         }
 
         // if the config file exists,
         // return true and store the path to it
+        info!("The config file exists");
         self.config_path = config_file_dir;
         Ok(true)
     }
@@ -93,13 +113,21 @@ impl Config {
     fn load_api_config(&mut self, config: &Ini) {
         // get the api_config section
         let api_config = match config.section(Some("Api")) {
-            Some(api_config) => api_config,
-            None => return,
+            Some(api_config) => {
+                info!("Found the Api Config");
+                api_config
+            }
+            None => {
+                info!("Api Config not found");
+                return;
+            }
         };
 
         // now load the settings
+        info!("Trying to load the BASE_URL");
         if api_config.get("BASE_URL").is_some() {
             self.api_config.base_url = api_config.get("BASE_URL").unwrap().to_string();
+            info!("Loaded the BASE_URL");
         }
     }
 }
