7b4e215fd47534db82415bc731f38b84fc2a69c7
Builditluc <37375448+Builditluc@users.noreply.github.com>
2022-09-29 08:55:40 +0200

    feat: add percentage utility function

--- src/home.rs
+++ src/home.rs
@@ -8,7 +8,7 @@ use cursive_aligned_view::Alignable;
 
 use crate::{
     config::CONFIG,
-    ui::{search::on_search, RootLayout},
+    ui::{search::on_search, utils::percentage, RootLayout},
     LOGO,
 };
 
@@ -18,17 +18,17 @@ const LOGO_PROMT_SPACING: usize = 5;
 pub fn display_home() -> Box<dyn FnOnce(&mut Cursive) + Send> {
     Box::new(move |siv| {
         let logo_view = TextView::new(LOGO).h_align(HAlign::Center).full_width();
-        let filler_view = DummyView::fixed_height(DummyView {}, LOGO_PROMT_SPACING);
+        let spacer_view = DummyView::fixed_height(DummyView {}, LOGO_PROMT_SPACING);
         let search_promt = Panel::new(EditView::new().on_submit(on_search))
             .title("Search")
-            .fixed_width((siv.screen_size().x as f32 * PROMT_WIDTH_PERCENTAGE) as usize)
+            .fixed_width(percentage(siv.screen_size().x, PROMT_WIDTH_PERCENTAGE))
             .align_center();
 
         siv.add_fullscreen_layer(
             Panel::new(
                 RootLayout::vertical(CONFIG.keybindings.clone())
                     .child(logo_view)
-                    .child(filler_view)
+                    .child(spacer_view)
                     .child(search_promt)
                     .input(true)
                     .full_screen(),
--- src/ui/utils.rs
+++ src/ui/utils.rs
@@ -1,19 +1,9 @@
-use crate::ui::RootLayout;
 use anyhow::Error;
 use cursive::{views::Dialog, Cursive};
 
-/// Removes a given view from a given layout. If the view or the layout couldn't be found, the
-/// function fails silently
-pub fn remove_view_from_layout(siv: &mut Cursive, view_name: &str, layout_name: &str) {
-    let result = siv.call_on_name(layout_name, |view: &mut RootLayout| {
-        if let Some(i) = view.find_child_from_name(view_name) {
-            log::debug!("removed '{}' from '{}'", view_name, layout_name);
-            view.remove_child(i);
-        }
-    });
-    if result.is_none() {
-        log::warn!("couldn't find a layout with the name '{}'", layout_name);
-    }
+/// Returns the percentage of a given value
+pub fn percentage(value: usize, percentage: f32) -> usize {
+    (value as f32 * percentage) as usize
 }
 
 /// Wraps a view into a ThemedView with the given theme. If the macro is used without a theme,
