e5ea1a16dfaacbcf07bed7b9506399e7f7bb5923
Builditluc <37375448+Builditluc@users.noreply.github.com>
2023-02-01 15:54:54 +0100

    refactor: create helper function for creating a query

--- src/wiki/api/mediawiki.rs
+++ src/wiki/api/mediawiki.rs
@@ -2,8 +2,9 @@ use reqwest::blocking::Client;
 
 #[derive(Debug)]
 pub enum Error {
-    HTTPError,
-    JSONError,
+    HTTPError(reqwest::Error),
+    JSONError(serde_json::Error),
+    JSONPathError(String),
 }
 
 #[derive(Debug)]
@@ -61,45 +62,45 @@ impl Mediawiki {
         }
     }
 
+    fn query(&self, parameters: &[(&str, &str)]) -> Result<serde_json::Value, Error> {
+        serde_json::from_str(
+            &self
+                .client
+                .get(self.url.to_owned())
+                .query(&[("format", "json")])
+                .query(parameters)
+                .send()
+                .map_err(Error::HTTPError)?
+                .text()
+                .map_err(Error::HTTPError)?,
+        )
+        .map_err(Error::JSONError)
+    }
+
     pub fn search(&self, query: &str) -> Result<Search, Error> {
         self.search_at_offset(query, 0)
     }
 
     pub fn search_at_offset(&self, query: &str, offset: u64) -> Result<Search, Error> {
-        let res_json: serde_json::Value = serde_json::from_str(
-            &self
-                .client
-                .get(self.url.to_owned())
-                .query(&[
-                    ("format", "json"),
-                    ("action", "query"),
-                    ("list", "search"),
-                    ("srsearch", query),
-                    ("sroffset", &offset.to_string()),
-                ])
-                .send()
-                .map_err(|_| Error::HTTPError)?
-                .text()
-                .map_err(|_| Error::HTTPError)?,
-        )
-        .map_err(|_| Error::JSONError)?;
+        let res_json = self.query(&[
+            ("action", "query"),
+            ("list", "search"),
+            ("srsearch", query),
+            ("sroffset", &offset.to_string()),
+        ])?;
         self.search_from_json(res_json)
     }
 
     fn search_from_json(&self, json: serde_json::Value) -> Result<Search, Error> {
         let search_offset = json
-            .as_object()
-            .ok_or(Error::JSONError)?
             .get("continue")
             .and_then(|x| x.get("sroffset"))
             .and_then(|x| x.as_u64())
             .map(|x| x.to_owned());
 
         let query_json = json
-            .as_object()
-            .ok_or(Error::JSONError)?
             .get("query")
-            .ok_or(Error::JSONError)?;
+            .ok_or(Error::JSONPathError("missing element '$.query'".into()))?;
 
         let search_info = SearchInfo {
             total_hits: query_json
@@ -119,12 +120,15 @@ impl Mediawiki {
         };
 
         // retrieve the search results
-        let search_results: Vec<SearchResult> =
-            serde_json::from_value(query_json.get("search").ok_or(Error::JSONError)?.to_owned())
-                .map_err(|x| {
-                    println!("{:?}", x);
-                    Error::JSONError
-                })?;
+        let search_results: Vec<SearchResult> = serde_json::from_value(
+            query_json
+                .get("search")
+                .ok_or(Error::JSONPathError(
+                    "missing element '$query.search'".into(),
+                ))?
+                .to_owned(),
+        )
+        .map_err(Error::JSONError)?;
 
         Ok(Search {
             offset: search_offset,
@@ -134,60 +138,43 @@ impl Mediawiki {
     }
 
     pub fn article_from_title(&self, title: &str) -> Result<Article, Error> {
-        let res_json: serde_json::Value = serde_json::from_str(
-            &self
-                .client
-                .get(self.url.to_owned())
-                .query(&[
-                    ("format", "json"),
-                    ("action", "parse"),
-                    ("page", title),
-                    ("prop", "sections|text"),
-                ])
-                .send()
-                .map_err(|_| Error::HTTPError)?
-                .text()
-                .map_err(|_| Error::HTTPError)?,
-        )
-        .map_err(|_| Error::JSONError)?;
-
+        let res_json = self.query(&[
+            ("action", "parse"),
+            ("page", title),
+            ("prop", "sections|text"),
+        ])?;
         self.article_from_json(res_json)
     }
 
     pub fn article_from_id(&self, id: u64) -> Result<Article, Error> {
-        let res_json: serde_json::Value = serde_json::from_str(
-            &self
-                .client
-                .get(self.url.to_owned())
-                .query(&[
-                    ("format", "json"),
-                    ("action", "parse"),
-                    ("pageid", &id.to_string()),
-                    ("prop", "sections|text"),
-                ])
-                .send()
-                .map_err(|_| Error::HTTPError)?
-                .text()
-                .map_err(|_| Error::HTTPError)?,
-        )
-        .map_err(|_| Error::JSONError)?;
-
+        let res_json = self.query(&[
+            ("format", "json"),
+            ("action", "parse"),
+            ("pageid", &id.to_string()),
+            ("prop", "sections|text"),
+        ])?;
         self.article_from_json(res_json)
     }
 
     fn article_from_json(&self, json: serde_json::Value) -> Result<Article, Error> {
         let parse_json = json
-            .as_object()
-            .ok_or(Error::JSONError)?
             .get("parse")
-            .ok_or(Error::JSONError)?;
+            .ok_or(Error::JSONPathError("missing element '$.parse'".into()))?;
 
-        let article_title = parse_json.get("title").ok_or(Error::JSONError)?.to_string();
+        let article_title = parse_json
+            .get("title")
+            .ok_or(Error::JSONPathError(
+                "missing element '$.parse.title'".into(),
+            ))?
+            .to_string();
 
-        let article_id = parse_json
-            .get("pageid")
-            .and_then(|x| x.as_u64())
-            .ok_or(Error::JSONError)?;
+        let article_id =
+            parse_json
+                .get("pageid")
+                .and_then(|x| x.as_u64())
+                .ok_or(Error::JSONPathError(
+                    "missing or invalid element '$.parse.pageid'".into(),
+                ))?;
 
         let article_text = parse_json
             .get("text")
