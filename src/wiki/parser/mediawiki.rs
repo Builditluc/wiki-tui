be6925b213a454e6b0b131680dc23977a3fcc7e6
Builditluc <37375448+Builditluc@users.noreply.github.com>
2023-02-04 10:07:03 +0100

    feat: add MediawikiUnsupportedElementParser

--- src/wiki/parser/mediawiki.rs
+++ src/wiki/parser/mediawiki.rs
@@ -1,3 +1,5 @@
+use cursive::theme::Effect;
+
 use crate::wiki::article_new::Section;
 
 use super::{
@@ -42,10 +44,7 @@ impl Parser for MediawikiParser {
 struct MediawikiParagraphParser;
 
 impl ElementParser for MediawikiParagraphParser {
-    fn parse_node(&self, node: select::node::Node, parser: &mut dyn Parser)
-    where
-        Self: Sized,
-    {
+    fn parse_node(&self, node: select::node::Node, parser: &mut dyn Parser) {
         for child in node.children() {
             if let Some(name) = child.name() {
                 let element_parser = parser.get_parser(name);
@@ -59,5 +58,17 @@ impl ElementParser for MediawikiParagraphParser {
 
             parser.push_element(Box::new(Text::new(id, content, effects)))
         }
+    }
+}
+
+struct MediawikiUnsupportedElementParser;
+
+impl ElementParser for MediawikiUnsupportedElementParser {
+    fn parse_node(&self, node: select::node::Node, parser: &mut dyn Parser) {
+        let id = parser.next_id();
+        let content = format!("<Unsupported Element '{}'>", node.name().unwrap_or("NONE"));
+        let effects = vec![Effect::Italic];
+
+        parser.push_element(Box::new(Text::new(id, content, effects)))
     }
 }
