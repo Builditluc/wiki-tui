8fce9929310a12a6e7ce1d498b60d37956458872
Builditluc <37375448+Builditluc@users.noreply.github.com>
2023-03-23 08:34:33 +0100

    add simple search bar (non working)

--- src/ui/language_selector.rs
+++ src/ui/language_selector.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use cursive::{view::Nameable, Cursive};
+use cursive::{view::Nameable, views::EditView, Cursive};
 
 use crate::{
     config::{Config, CONFIG},
@@ -14,6 +14,13 @@ use super::{
 };
 
 const POPUP_NAME: &str = "language_selection_popup";
+const LANGUAGE_SELECTION_NAME: &str = "language_selection";
+
+fn change_lang(siv: &mut Cursive, lang: impl Into<Language>) {
+    siv.with_user_data(|c: &mut Rc<RefCell<Config>>| {
+        c.borrow_mut().api_config.language = lang.into();
+    });
+}
 
 /// Displays a popup that lets the user chosse a new language
 pub fn language_selection_popup(siv: &mut Cursive) {
@@ -22,18 +29,22 @@ pub fn language_selection_popup(siv: &mut Cursive) {
         return;
     }
 
-    let mut language_selection: SelectView<&Language> =
-        SelectView::new().on_submit(|s, item: &Language| {
+    let language_search = EditView::new();
+
+    let mut language_selection: SelectView<String> =
+        SelectView::new().on_submit(|s, item: &String| {
             s.pop_layer();
-            s.with_user_data(|c: &mut Rc<RefCell<Config>>| {
-                c.borrow_mut().api_config.language = item.to_owned();
-            });
-            warn!("failed updating the language: configuration not found")
+            change_lang(s, item.as_str());
         });
-    language_selection.add_all(LANGUAGES.iter().map(|l| (l.name(), l)));
+    language_selection.add_all(LANGUAGES.iter().map(|l| (l.name(), l.code().to_owned())));
     siv.add_layer(
         RootLayout::vertical(CONFIG.keybindings.clone())
-            .child(language_selection.scrollable())
+            .child(language_search)
+            .child(
+                language_selection
+                    .scrollable()
+                    .with_name(LANGUAGE_SELECTION_NAME),
+            )
             .input(true)
             .with_name(POPUP_NAME)
             .with_panel()
