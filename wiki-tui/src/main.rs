3af7e2895717c87a1297c38dd5fae52dc9233aa7
Builditluc <37375448+Builditluc@users.noreply.github.com>
2023-09-03 16:21:00 +0200

    feat: add tui handler

--- wiki-tui/src/main.rs
+++ wiki-tui/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, world!");
+    Ok(())
 }
---
+++ wiki-tui/src/terminal.rs
@@ -0,0 +1,54 @@
+use anyhow::{Context, Result};
+use crossterm::{
+    cursor,
+    event::{DisableMouseCapture, EnableMouseCapture},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::prelude::CrosstermBackend as Backend;
+
+pub type Frame<'a> = ratatui::Frame<'a, Backend<std::io::Stderr>>;
+
+pub struct Tui {
+    pub terminal: ratatui::Terminal<Backend<std::io::Stderr>>,
+}
+
+impl Tui {
+    pub fn new() -> Result<Self> {
+        let terminal = ratatui::Terminal::new(Backend::new(std::io::stderr()))
+            .context("unable to create terminal")?;
+        Ok(Self { terminal })
+    }
+
+    pub fn enter(&self) -> Result<()> {
+        crossterm::terminal::enable_raw_mode()?;
+        crossterm::execute!(
+            std::io::stderr(),
+            EnterAlternateScreen,
+            EnableMouseCapture,
+            cursor::Hide
+        )?;
+        Ok(())
+    }
+
+    pub fn exit(&self) -> Result<()> {
+        crossterm::execute!(
+            std::io::stderr(),
+            LeaveAlternateScreen,
+            DisableMouseCapture,
+            cursor::Show
+        )?;
+        crossterm::terminal::enable_raw_mode()?;
+        Ok(())
+    }
+
+    pub fn suspend(&self) -> Result<()> {
+        self.exit()?;
+        signal_hook::low_level::raise(signal_hook::consts::signal::SIGTSTP)?;
+        Ok(())
+    }
+
+    pub fn resume(&self) -> Result<()> {
+        self.enter()?;
+        Ok(())
+    }
+}
