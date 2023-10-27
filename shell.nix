d7c4becd3cc948f7736e2518c78ed444e8eec2e1
Builditluc <37375448+Builditluc@users.noreply.github.com>
2023-10-27 20:59:03 +0200

    fix nix-shell and ignore logs in git

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 pkgs.mkShell {
     nativeBuildInputs = with pkgs; [ libiconv rustup ];
 
-    buildInputs = with pkgs; [ openssl ncurses ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+    buildInputs = with pkgs; [ openssl ncurses pkg-config ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
     RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
