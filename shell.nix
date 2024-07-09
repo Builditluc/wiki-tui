c74d03fa4e955f9f9128913ac0166c7f2b3019ec
Builditluc <37375448+Builditluc@users.noreply.github.com>
2024-07-09 16:04:35 +0200

    chore(*): add gdb package to nix-shell

--- shell.nix
+++ shell.nix
@@ -3,6 +3,8 @@ pkgs.mkShell {
     nativeBuildInputs = with pkgs; [ libiconv rustup ];
 
     buildInputs = with pkgs; [ openssl ncurses pkg-config ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+    
+    packages = [ pkgs.gdb ];
 
     RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
